dfa5fbbe535eff6b0c226e44d86efaa0d912cddf
Oleg Toporkov <74182627+lezhocheck@users.noreply.github.com>
2021-10-14 12:37:06 +0300

    Update program.cpp

--- review_1/program.cpp
+++ review_1/program.cpp
@@ -3,86 +3,122 @@
 
 constexpr int MOD = 1'000'000'007;
 
-const std::vector<int64_t>& Matrix::get_row(size_t i) const {
-    return _matrix.at(i);
+class Matrix{
+ public:
+     using vi = std::vector<int64_t>;
+     Matrix(const size_t n, const size_t m) :
+             matrix_(std::vector(n, vi(m, 0))){}
+
+     const vi& GetRow(const size_t row_number) const;
+     const int64_t GetElement(const size_t row_number,
+                              const size_t column_number) const;
+     void SetElement(const size_t row_number,
+                     const size_t column_number, const int64_t value);
+
+     const size_t GetXSize() const;
+     const size_t GetYSize() const;
+
+     Matrix operator* (const Matrix& other) const;
+     Matrix ElevateToPower(size_t power) const;
+
+ private:
+     std::vector<vi> matrix_;
+};
+
+const std::vector<int64_t>& Matrix::GetRow(const size_t row_number) const {
+    return matrix_.at(row_number);
 }
 
-int64_t& Matrix::at(size_t i, size_t j) {
-    return _matrix.at(i).at(j);
+const int64_t Matrix::GetElement(const size_t row_number,
+                                 const size_t column_number) const {
+    return matrix_.at(row_number).at(column_number);
 }
 
-size_t Matrix::x_size() const {
-    return _matrix.size();
+void Matrix::SetElement(const size_t row_number,
+                        const size_t column_number, const int64_t value){
+    matrix_.at(row_number).at(column_number) = value;
 }
 
-size_t Matrix::y_size() const {
-    return _matrix[0].size();
+const size_t Matrix::GetXSize() const {
+    return matrix_.size();
+}
+
+const size_t Matrix::GetYSize() const {
+    return matrix_[0].size();
 }
 
 Matrix Matrix::operator* (const Matrix& other) const {
-    size_t n = x_size();
-    size_t m = y_size();
-    size_t other_n = other.x_size();
-    size_t other_m = other.y_size();
+    size_t x_size = GetXSize();
+    size_t y_size = GetYSize();
+    size_t other_x_size = other.GetXSize();
+    size_t other_y_size = other.GetYSize();
 
-    if (m != other_n) {
+    if (y_size != other_x_size) {
         throw std::invalid_argument("Matrices sizes mismatch");
     }
 
-    Matrix res(n, other_m);
+    Matrix result_matrix(x_size, other_y_size);
 
-    for (size_t i = 0; i < n; ++i) {
-        for (size_t j = 0; j < other_m; ++j) {
-            int64_t s = 0;
-            for (size_t k = 0; k < n; ++k) {
-                s = (s + (_matrix[i][k] * other._matrix[k][j]) % MOD) % MOD;
+    for (size_t row = 0; row < x_size; ++row) {
+        for (size_t column = 0; column < other_y_size; ++column) {
+            int64_t new_element = 0;
+            for (size_t k = 0; k < x_size; ++k) {
+                new_element = (new_element + (matrix_[row][k] *
+                    other.matrix_[k][column]) % MOD) % MOD;
             }
-            res.at(i, j) = s;
+            result_matrix.SetElement(row, column, new_element);
         }
     }
-    return res;
+    return result_matrix;
 }
 
-Matrix Matrix::bin_pow(size_t k) const {
-    size_t n = x_size();
-    Matrix temp = *this;
-    Matrix res(n, n);
+Matrix Matrix::ElevateToPower(size_t power) const {
+    size_t x_size = GetXSize();
+    Matrix this_matrix = *this;
+    Matrix result_matrix(x_size, x_size);
 
-    for (size_t i = 0; i < n; ++i) {
-        res.at(i, i) = 1;
+    for (size_t row = 0; row < x_size; ++row) {
+        result_matrix.SetElement(row, row, 1);
     }
 
-    while (k > 0) {
-        if (k % 2 == 1) {
-            res = temp * res;
+    while (power > 0) {
+        if (power % 2 == 1) {
+            result_matrix = this_matrix * result_matrix;
         }
-        temp = temp * temp;
-        k /= 2;
+        this_matrix = this_matrix * this_matrix;
+        power /= 2;
     }
-    return res;
+    return result_matrix;
 }
 
-Matrix create_adj(std::istream& in, int vertices, int edges) {
-    Matrix res(vertices, vertices);
+Matrix InitializeMatrix(std::istream& in, int* power) {
+    int vertices, edges;
+    in >> vertices >> edges >> *power;
+    Matrix result_matrix(vertices, vertices);
 
-    for (int i = 0; i < edges; ++i) {
-        int a, b;
-        in >> a >> b;
-        a--;
-        b--;
-        res.at(a, b)++;
+    for (int edge = 0; edge < edges; ++edge) {
+        int index_first, index_second;
+        in >> index_first >> index_second;
+        index_first--;
+        index_second--;
+        result_matrix.SetElement(index_first, index_second,
+            result_matrix.GetElement(index_first, index_second) + 1);
     }
 
-    return res;
+    return result_matrix;
 }
 
-std::ostream& write_res(std::ostream& out, const Matrix& m) {
-    int64_t ans = 0;
+Matrix Solve(const Matrix& matrix, int power) {
+    return matrix.ElevateToPower(power);
+}
 
-    for (int64_t i : m.get_row(0)){
-        ans += (i % MOD);
+std::ostream& WriteResult(std::ostream& out, const Matrix& matrix) {
+    int64_t answer = 0;
+
+    for (int64_t value : matrix.GetRow(0)){
+        answer += (value % MOD);
     }
 
-    out << ans % MOD;
+    out << answer % MOD << "\n";
     return out;
 }
