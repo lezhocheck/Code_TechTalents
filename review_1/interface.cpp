4008ac0ed1cda26ef70e94972e7ce92ddf52053a
Oleg Toporkov <74182627+lezhocheck@users.noreply.github.com>
2021-10-16 12:28:28 +0300

    Update interface.cpp

--- review_1/interface.cpp
+++ review_1/interface.cpp
@@ -3,42 +3,31 @@
 
 class SquareMatrix{
 public:
-    SquareMatrix(const size_t size) {
-        matrix_ = std::vector(size, std::vector<int64_t>(size, 0));
-    }
-    // получить ряд матрицы
-    const std::vector<int64_t>& getRow(const size_t rowNumber) const;
-    // получить элемент по индексам
-    const int64_t getElement(const size_t rowNumber,
-                             const size_t columnNumber) const;
-    // назначить элемент по индексам
-    void setElement(const size_t rowNumber,
-                    const size_t columnNumber, const int64_t value);
+    SquareMatrix(const size_t size);
+    SquareMatrix(const std::vector<std::vector<int64_t>>& matrix);
+
     SquareMatrix operator* (const SquareMatrix& other) const;
-    // возвести матрицу в степень
     SquareMatrix elevateToPower(size_t power) const;
+
+    friend std::ostream& write(std::ostream& out, const SquareMatrix& matrix);
 
 private:
     std::vector<std::vector<int64_t>> matrix_;
 };
 
-struct SquareMatrixPower {
-    SquareMatrix matrix;
+struct VectorPower {
+    std::vector<std::vector<int64_t>> base;
     int power;
 };
 
-// инициализация матрицы смежности графа
-SquareMatrixPower read(std::istream& in);
-// посчитать ответ
-SquareMatrix solve(const SquareMatrixPower& squareMatrixPower);
-// вывод количества путей заданной длины из 0 вершины
-std::ostream& write(std::ostream& out, const SquareMatrix& matrix);
+VectorPower read(std::istream& in);
+SquareMatrix solve(const VectorPower& vectorPower);
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
-    const SquareMatrixPower squareMatrixPower = read(std::cin);
+    const VectorPower squareMatrixPower = read(std::cin);
     const SquareMatrix resultMatrix = solve(squareMatrixPower);
     write(std::cout, resultMatrix);
 
