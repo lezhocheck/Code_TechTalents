7d254fb88487a5d56d371339e4612c4b894a1c01
Oleg Toporkov <74182627+lezhocheck@users.noreply.github.com>
2021-10-16 11:48:27 +0300

    Update interface.cpp

--- review_1/interface.cpp
+++ review_1/interface.cpp
@@ -1,53 +1,47 @@
 #include <iostream>
 #include <vector>
 
-class Matrix{
+class SquareMatrix{
 public:
-    Matrix(const size_t row_count, const size_t column_count) :
-            matrix_(std::vector(row_count,
-                                std::vector<int64_t>(column_count, 0))){}
+    SquareMatrix(const size_t size) {
+        matrix_ = std::vector(size, std::vector<int64_t>(size, 0));
+    }
     // получить ряд матрицы
-    const std::vector<int64_t>& get_row(const size_t row_number) const;
+    const std::vector<int64_t>& getRow(const size_t rowNumber) const;
     // получить элемент по индексам
-    const int64_t get_element(const size_t row_number,
-                              const size_t column_number) const;
-    // назначить новое значение элементу по индексам
-    void set_element(const size_t row_number,
-                     const size_t column_number, const int64_t value);
-
-    // получить глубину матрицы
-    const size_t get_x_size() const;
-    // получить ширину матрицы
-    const size_t get_y_size() const;
-
-    Matrix operator* (const Matrix& other) const;
+    const int64_t getElement(const size_t rowNumber,
+                             const size_t columnNumber) const;
+    // назначить элемент по индексам
+    void setElement(const size_t rowNumber,
+                    const size_t columnNumber, const int64_t value);
+    SquareMatrix operator* (const SquareMatrix& other) const;
     // возвести матрицу в степень
-    Matrix elevate_to_power(size_t power) const;
+    SquareMatrix elevateToPower(size_t power) const;
 
 private:
     std::vector<std::vector<int64_t>> matrix_;
 };
 
-struct MatrixPower{
-    Matrix matrix;
+struct SquareMatrixPower {
+    SquareMatrix matrix;
     int power;
 };
 
 // инициализация матрицы смежности графа
-MatrixPower read(std::istream& in);
+SquareMatrixPower read(std::istream& in);
 // посчитать ответ
-Matrix solve(const MatrixPower& matrix_power);
+SquareMatrix solve(const SquareMatrixPower& squareMatrixPower);
 // вывод количества путей заданной длины из 0 вершины
-std::ostream& write(std::ostream& out, const Matrix& matrix);
+std::ostream& write(std::ostream& out, const SquareMatrix& matrix);
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
-    const MatrixPower base_matrix = read(std::cin);
-    const Matrix result_matrix = solve(base_matrix);
-    write(std::cout, result_matrix);
-    
+    const SquareMatrixPower squareMatrixPower = read(std::cin);
+    const SquareMatrix resultMatrix = solve(squareMatrixPower);
+    write(std::cout, resultMatrix);
+
     return 0;
 }
 
