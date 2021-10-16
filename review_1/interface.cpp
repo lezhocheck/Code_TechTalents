ae9e6fd7cb03105b3554d7404a77221ff3911288
Oleg Toporkov <74182627+lezhocheck@users.noreply.github.com>
2021-10-16 14:07:52 +0300

    Update interface.cpp

--- review_1/interface.cpp
+++ review_1/interface.cpp
@@ -8,7 +8,7 @@ public:
     explicit SquareMatrix(const size_t size);
     explicit SquareMatrix(const std::vector<std::vector<int64_t>>& matrix);
 
-    SquareMatrix operator* (const SquareMatrix& other) const;
+    SquareMatrix operator* (const SquareMatrix& secondMatrix) const;
     SquareMatrix elevateToPower(size_t power) const;
 
     const std::vector<int64_t>& getRow(const size_t rowNumber) const;
@@ -18,21 +18,21 @@ private:
 };
 
 struct LabyrinthData {
-    std::vector<std::vector<int64_t>> base;
-    int power;
+    std::vector<std::vector<int64_t>> labyrinth;
+    int pathLength;
 };
 
 LabyrinthData readLabyrinthData(std::istream& in);
-int64_t getNumberOfPathsFromStart(const LabyrinthData& vectorPower);
-std::ostream& writeNumberOfPathsFromStart(std::ostream& out, const int64_t value);
+int64_t getNumberOfPaths(const LabyrinthData& labyrinthData);
+std::ostream& writeNumberOfPaths(std::ostream& out, const int64_t value);
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
     const LabyrinthData labyrinthData = readLabyrinthData(std::cin);
-    const int64_t numberOfPathsFromStart = getNumberOfPathsFromStart(labyrinthData);
-    writeNumberOfPathsFromStart(std::cout, numberOfPathsFromStart);
+    const int64_t numberOfPaths = getNumberOfPaths(labyrinthData);
+    writeNumberOfPaths(std::cout, numberOfPaths);
 
     return 0;
 }
