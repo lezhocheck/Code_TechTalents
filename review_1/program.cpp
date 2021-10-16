b597fc18f272c49d50896d5d79bf33142468b496
Oleg Toporkov <74182627+lezhocheck@users.noreply.github.com>
2021-10-16 14:08:31 +0300

    Update program.cpp

--- review_1/program.cpp
+++ review_1/program.cpp
@@ -3,25 +3,6 @@
 
 const int MODULUS = 1'000'000'007;
 
-class SquareMatrix{
- public:
-     explicit SquareMatrix(const size_t size);
-     explicit SquareMatrix(const std::vector<std::vector<int64_t>>& matrix);
-
-     SquareMatrix operator* (const SquareMatrix& other) const;
-     SquareMatrix elevateToPower(size_t power) const;
-
-    const std::vector<int64_t>& getRow(const size_t rowNumber) const;
-
- private:
-     std::vector<std::vector<int64_t>> matrix_;
-};
-
-struct LabyrinthData {
-    std::vector<std::vector<int64_t>> base;
-    int power;
-};
-
 const std::vector<int64_t>& SquareMatrix::getRow(const size_t rowNumber) const{
     return matrix_.at(rowNumber);
 }
@@ -35,22 +16,22 @@ SquareMatrix::SquareMatrix(const std::vector<std::vector<int64_t>>& matrix) {
     matrix_ = matrix;
 }
 
-SquareMatrix SquareMatrix::operator* (const SquareMatrix& other) const {
-    size_t matrixSize = matrix_.size();
-    size_t otherMatrixSize = other.matrix_.size();
+SquareMatrix SquareMatrix::operator* (const SquareMatrix& secondMatrix) const {
+    size_t firstMatrixSize = matrix_.size();
+    size_t secondMatrixSize = secondMatrix.matrix_.size();
 
-    if (matrixSize != otherMatrixSize) {
+    if (firstMatrixSize != secondMatrixSize) {
         throw std::invalid_argument("matrices sizes mismatch");
     }
 
-    SquareMatrix resultMatrix(matrixSize);
+    SquareMatrix resultMatrix(firstMatrixSize);
 
-    for (size_t row = 0; row < matrixSize; ++row) {
-        for (size_t column = 0; column < matrixSize; ++column) {
+    for (size_t row = 0; row < firstMatrixSize; ++row) {
+        for (size_t column = 0; column < firstMatrixSize; ++column) {
             int64_t newElement = 0;
-            for (size_t k = 0; k < matrixSize; ++k) {
+            for (size_t k = 0; k < firstMatrixSize; ++k) {
                 newElement = (newElement + (matrix_[row][k] *
-                    other.matrix_[k][column]) % MODULUS) % MODULUS;
+                    secondMatrix.matrix_[k][column]) % MODULUS) % MODULUS;
             }
             resultMatrix.matrix_[row][column] = newElement;
         }
@@ -60,7 +41,7 @@ SquareMatrix SquareMatrix::operator* (const SquareMatrix& other) const {
 
 SquareMatrix SquareMatrix::elevateToPower(size_t power) const {
     size_t matrixSize = matrix_.size();
-    SquareMatrix thisMatrix = *this;
+    SquareMatrix matrix = *this;
     SquareMatrix resultMatrix(matrixSize);
     for (size_t row = 0; row < matrixSize; ++row) {
         resultMatrix.matrix_[row][row] = 1;
@@ -68,9 +49,9 @@ SquareMatrix SquareMatrix::elevateToPower(size_t power) const {
 
     while (power > 0) {
         if (power % 2 == 1) {
-            resultMatrix = thisMatrix * resultMatrix;
+            resultMatrix = matrix * resultMatrix;
         }
-        thisMatrix = thisMatrix * thisMatrix;
+        matrix = matrix * matrix;
         power /= 2;
     }
     return resultMatrix;
@@ -93,30 +74,19 @@ LabyrinthData readLabyrinthData(std::istream& in) {
     return {base, power};
 }
 
-int64_t getNumberOfPathsFromStart(const LabyrinthData& vectorPower) {
-    SquareMatrix matrix(vectorPower.base);
-    SquareMatrix powered = matrix.elevateToPower(vectorPower.power);
-    int64_t answer = 0;
+int64_t getNumberOfPaths(const LabyrinthData& labyrinthData) {
+    SquareMatrix matrix(labyrinthData.labyrinth);
+    SquareMatrix powered = matrix.elevateToPower(labyrinthData.pathLength);
+    int64_t numberOfPaths = 0;
 
     for (int64_t value : powered.getRow(0)) {
-        answer += (value % MODULUS);
+        numberOfPaths += (value % MODULUS);
     }
 
-    return (answer % MODULUS);
+    return (numberOfPaths % MODULUS);
 }
 
-std::ostream& writeNumberOfPathsFromStart(std::ostream& out, const int64_t value) {
+std::ostream& writeNumberOfPaths(std::ostream& out, const int64_t value) {
     out << value << "\n";
     return out;
-}
-
-int main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    const LabyrinthData squareMatrixPower = readLabyrinthData(std::cin);
-    const int64_t numberOfPathsFromStart = getNumberOfPathsFromStart(squareMatrixPower);
-    writeNumberOfPathsFromStart(std::cout, numberOfPathsFromStart);
-
-    return 0;
 }
