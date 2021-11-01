c9a41f05a2e965ce25842e8facc072a3c41d227b
Oleg Toporkov <74182627+lezhocheck@users.noreply.github.com>
2021-11-01 08:30:27 +0200

    Update program.cpp

--- review_1/program.cpp
+++ review_1/program.cpp
@@ -3,6 +3,25 @@
 
 const int MODULUS = 1'000'000'007;
 
+class SquareMatrix{
+public:
+    explicit SquareMatrix(const size_t size);
+    explicit SquareMatrix(const std::vector<std::vector<int64_t>>& matrix);
+
+    SquareMatrix operator* (const SquareMatrix& secondMatrix) const;
+    SquareMatrix elevateToPower(size_t power) const;
+
+    const std::vector<int64_t>& getRow(const size_t rowNumber) const;
+
+private:
+    std::vector<std::vector<int64_t>> matrix_;
+};
+
+struct LabyrinthData {
+    std::vector<std::vector<int64_t>> labyrinth;
+    int pathLength;
+};
+
 const std::vector<int64_t>& SquareMatrix::getRow(const size_t rowNumber) const{
     return matrix_.at(rowNumber);
 }
@@ -90,3 +109,15 @@ std::ostream& writeNumberOfPaths(std::ostream& out, const int64_t value) {
     out << value << "\n";
     return out;
 }
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    const LabyrinthData labyrinthData = readLabyrinthData(std::cin);
+    const int64_t numberOfPaths = getNumberOfPaths(labyrinthData);
+    writeNumberOfPaths(std::cout, numberOfPaths);
+
+    return 0;
+}
+
