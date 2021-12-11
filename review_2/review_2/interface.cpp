1db900f5250a3f13eeda120f1601b34b9a863507
Oleg Toporkov <74182627+lezhocheck@users.noreply.github.com>
2021-12-11 16:44:31 +0200

    Add files via upload

---
+++ review_2/review_2/interface.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+const char POSITIVE_ANSWER[] = "YES";
+const char NEGATIVE_ANSWER[] = "NO";
+
+struct Query {
+    int firstBox;
+    int secondBox;
+    int difference;
+};
+
+class DisjointSetUnion{
+public:
+    explicit DisjointSetUnion(size_t size);
+    bool unionSets(const Query& query);
+    size_t size() const;
+    int getDifference(int index);
+
+private:
+    struct Set {
+        Set* parent;
+        int rank;
+        int differenceWithMinimum;
+        int differenceWithParent;
+        bool isStart;
+    };
+
+    std::vector<Set> sets;
+
+    void makeSet(int index);
+    Set* findSet(Set* set);
+    bool merge(Set* firstSet, Set* secondSet, int difference);
+};
+
+struct BoxesAndNotes{
+    DisjointSetUnion boxes;
+    std::vector<Query> notes;
+};
+
+struct Answer{
+    bool isPossible;
+    uint64_t notPossibleAfter;
+    std::vector<int> result;
+};
+
+BoxesAndNotes readInput(std::istream& istream);
+Answer calculateAnswer(const BoxesAndNotes& boxesAndNotes);
+void printResult(std::ostream& ostream, const Answer& answer);
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+    const BoxesAndNotes boxesAndNotes = readInput(std::cin);
+    const Answer answer = calculateAnswer(boxesAndNotes);
+    printResult(std::cout, answer);
+    return 0;
+}
