06cda8a1191eb51df55d3a38aafa47840daa1ffe
Oleg Toporkov <74182627+lezhocheck@users.noreply.github.com>
2021-12-14 14:16:10 +0200

    Update main.cpp

--- review_2/main.cpp
+++ review_2/main.cpp
@@ -5,46 +5,6 @@
 const char POSITIVE_ANSWER[] = "YES";
 const char NEGATIVE_ANSWER[] = "NO";
 
-struct Query {
-    int firstBox;
-    int secondBox;
-    int difference;
-};
-
-class DisjointSetUnion {
- public:
-    explicit DisjointSetUnion(const size_t size);
-    bool unionSets(const Query& query);
-    size_t size() const;
-    int getDifference(const int index);
-
- private:
-    struct Set {
-        Set* parent;
-        int rank;
-        int differenceWithMinimum;
-        int differenceWithParent;
-        bool isStart;
-    };
-
-    std::vector<Set> sets;
-
-    void makeSet(const int index);
-    Set* findSet(Set* set);
-    bool merge(Set* firstSet, Set* secondSet, const int difference);
-};
-
-struct CoinsDistributionData {
-    DisjointSetUnion boxes;
-    std::vector<Query> notes;
-};
-
-struct DistributionPossibilityResult {
-    bool isPossible;
-    uint64_t notPossibleAfter;
-    std::vector<int> result;
-};
-
 DisjointSetUnion::DisjointSetUnion(const size_t size) {
     sets = std::vector<Set>(size);
     for (int i = 0; i < size; i++) {
