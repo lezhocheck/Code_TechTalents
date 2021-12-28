673aa6f0d98031e8ddd08d61cf9a1cd0649defa3
Oleg Toporkov <74182627+lezhocheck@users.noreply.github.com>
2021-12-28 22:13:43 +0200

    Update interface.cpp

--- review_3/interface.cpp
+++ review_3/interface.cpp
@@ -14,19 +14,20 @@ public:
     explicit SegmentTree2D(const std::vector<std::vector<int16_t>>& initialMatrix);
 
     int16_t query(const int16_t fromColumn, const int16_t toColumn,
-        const int16_t fromRow, const int16_t toRow) const;
+    const int16_t fromRow, const int16_t toRow) const;
 
-    void update(const int16_t column, const int16_t row, const int16_t newValue);
+    void update(const int16_t column,
+    const int16_t row, const int16_t newValue);
 
     int16_t columnsSize() const;
-
     int16_t rowsSize() const;
 
 private:
     std::vector<std::vector<int16_t>> segmentTree2D;
     std::vector<std::vector<int16_t>> matrix;
 
-    struct Point {
+    class Point {
+    public:
         int16_t x;
         int16_t y;
 
@@ -36,11 +37,10 @@ private:
     struct Rectangle {
         Point bottomLeft;
         Point topRight;
-
-        Rectangle(const Point& bottomLeftValue, const Point& topRightValue);
     };
 
-    struct Range {
+    class Range {
+    public:
         int16_t lowerBound;
         int16_t upperBound;
 
@@ -51,8 +51,8 @@ private:
 
     // builds segment tree along the first (x) axis.
     void build(std::vector<int16_t>* segmentTree,
-        const std::vector<int16_t>& array,
-        const int16_t index, const Range& range);
+               const std::vector<int16_t>& array,
+               const int16_t index, const Range& range);
 
     // builds final version of segment tree along the second (y) axis.
     // call this function to build segment tree.
@@ -60,55 +60,59 @@ private:
 
     // finds maximum value in segment tree along the first (x) axis.
     int16_t query(const std::vector<int16_t>& segmentTree, const int16_t index,
-        const Range& queryRange, const Range& fixedRange) const;
+                  const Range& queryRange, const Range& fixedRange) const;
 
     // finds maximum value in segment tree along the second (y) axis and returns final result.
     // call this function to get maximum value in the rectangle.
     int16_t query2D(const int16_t index,
-        const Range& columnsRange, const Rectangle& rect) const;
+                    const Range& columnsRange, const Rectangle& rect) const;
 
     // updates segment tree along the first (x) axis.
     void update(const Range& columnRange,
-        const Range& rowRange, const int16_t indexX,
-        const int16_t indexY,
-        const Point& point, const int16_t value);
+                const Range& rowRange, const int16_t indexX,
+                const int16_t indexY,
+                const Point& point, const int16_t value);
 
     // updates segment tree along the second (y) axis.
     // call this function to update value in the tree.
     void update2D(const int16_t index, const Range& columnRange,
-        const Point& point, const int16_t value);
+                  const Point& point, const int16_t value);
 };
 
-struct QueryData {
+class QueryData {
+public:
     virtual QueryType getType() = 0;
 };
 
-struct MaxQueryData : public QueryData {
+class MaxQueryData : public QueryData {
+public:
     int16_t fromColumn, toColumn;
     int16_t fromRow, toRow;
 
     MaxQueryData(const int16_t fromColumnValue, const int16_t toColumnValue,
-        const int16_t fromRowValue, const int16_t toRowValue);
+    const int16_t fromRowValue, const int16_t toRowValue);
 
     QueryType getType() override;
 };
 
-struct UpdateQueryData : public QueryData {
+class UpdateQueryData : public QueryData {
+public:
     int16_t column, row;
     int16_t newValue;
 
     UpdateQueryData(const int16_t columnValue, const int16_t rowValue,
-        const int16_t value);
+    const int16_t value);
 
     QueryType getType() override;
 };
 
-struct MatrixQueryData {
+class MatrixQueryData {
+public:
     std::vector<std::vector<int16_t>> matrix;
     std::vector<QueryData*> queryData;
 
     MatrixQueryData(const int16_t columnsSize,
-        const int16_t rowsSize, const int queriesSize);
+    const int16_t rowsSize, const int queriesSize);
 };
 
 MatrixQueryData readMatrixQueryData(std::istream& istream);
